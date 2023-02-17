ad1da228466148dbdef6c2afe934cd9a3809139c
root <x-uzia>
2023-02-17 00:33:53 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using the printf function
- * Return: 0 (success)
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-print("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
